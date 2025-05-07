3713ddbd3e1077bf2271ca99c697f4475a3993a4
Willy <wacb14@gmail.com>
2025-05-07 11:08:11 -0500

    Create new element / POST and GetById / GET

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,46 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Product } from './models/Product';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { ProductService } from './services/product.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   productService = inject(ProductService);
+  fb = inject(FormBuilder);
+  productForm = this.fb.group({
+    name: ['', Validators.required],
+    price: [0, [Validators.required, Validators.min(0)]],
+    description: [''],
+  });
   title = 'couchDB-learning';
   products: Product[] = [];
 
   ngOnInit() {
+    this.loadProducts();
+  }
+  loadProducts() {
     this.productService.getProducts().subscribe((res: any) => {
-      this.products = res.rows.map((row: any) => row.doc);
+      if (res.rows) {
+        this.products = res.rows.map((row: any) => row.doc);
+      }
     });
+  }
+
+  addProduct() {
+    if (this.productForm.valid) {
+      this.productService
+        .addProduct(this.productForm.value)
+        .subscribe((res: any) => {
+          this.productService.getById(res.id).subscribe((res2) => {
+            this.products.push(<Product>res2);
+          });
+        });
+    }
+    this.productForm.reset();
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,20 +1,34 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Product } from '../models/Product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   http = inject(HttpClient);
-
   headers = new HttpHeaders({
     Authorization: 'Basic ' + btoa('admin:admin'),
     'Content-Type': 'application/json',
   });
+
+  getById(id:string): Observable<any> {
+    return this.http.get<any>(
+      `http://localhost:5984/products/${id}`,
+      { headers: this.headers }
+    );
+  }
   getProducts(): Observable<any> {
     return this.http.get<any>(
       'http://localhost:5984/products/_all_docs?include_docs=true',
+      { headers: this.headers }
+    );
+  }
+  addProduct(product: any): Observable<any> {
+    return this.http.post<any>(
+      'http://localhost:5984/products/',
+      product,
       { headers: this.headers }
     );
   }
